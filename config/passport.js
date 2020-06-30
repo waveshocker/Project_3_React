2c0d459b73ca133d0713853d1b50f04800de0b62
unknown <binhu10@gmail.com>
2020-06-29 20:59:24 -0400

    framework for db connection

--- config/passport.js
+++ config/passport.js
@@ -15,21 +15,21 @@ passport.use(new LocalStrategy(
       where: {
         email: email
       }
-    }).then(function(dbUser) {
+    }).then(function(dbDiner) {
       // If there's no user with the given email
-      if (!dbUser) {
+      if (!dbDiner) {
         return done(null, false, {
           message: "Incorrect email."
         });
       }
       // If there is a user with the given email, but the password the user gives us is incorrect
-      else if (!dbUser.validPassword(password)) {
+      else if (!dbDiner.validPassword(password)) {
         return done(null, false, {
           message: "Incorrect password."
         });
       }
       // If none of the above, return the user
-      return done(null, dbUser);
+      return done(null, dbDiner);
     });
   }
 ));
@@ -37,8 +37,8 @@ passport.use(new LocalStrategy(
 // In order to help keep authentication state across HTTP requests,
 // Sequelize needs to serialize and deserialize the user
 // Just consider this part boilerplate needed to make it all work
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
+passport.serializeUser(function(diner, cb) {
+  cb(null, diner);
 });
 
 passport.deserializeUser(function(obj, cb) {
